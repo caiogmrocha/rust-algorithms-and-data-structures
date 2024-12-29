dfbccbfa2f559d9536a4a2a59e2d3d07554ef4be
caiogmrocha <caioguilherme739@gmail.com>
2024-12-29 18:52:21 -0300

    feat(tokio-playground): initial tests

--- tokio-playground/src/main.rs
+++ tokio-playground/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::time::{sleep, Duration};
+use tokio::spawn;
+
+#[tokio::main]
+async fn main() {
+    let wait1 = spawn(wait_some_seconds_my_brother(Duration::from_secs(2)));
+    let wait2 = spawn(wait_some_seconds_my_brother(Duration::from_secs(4)));
+
+    if let Err(error) = wait1.await {
+        eprintln!("That's a shame my brother, wait1 failed {error}");
+    }
+
+    if let Err(error) = wait2.await {
+        eprintln!("That's a shame my brother, wait2 failed {error}");
+    }
+
+    println!("Ended my brother");
+}
+
+async fn wait_some_seconds_my_brother(duration: Duration) {
+    sleep(duration).await;
+
+    println!("waited {duration:?} ma brother");
 }
